57e6cc13d9e508d7289fcc8aaccb03fcf711dc33
Nick <76410793+nicholasaltieri@users.noreply.github.com>
2022-06-10 23:54:05 -0400

    initial

---
+++ frontend/src/pages/ReviewDetails.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useQuery, gql } from '@apollo/client'
+
+const REVIEW = gql`
+query GetReview($id: ID!) {
+
+  review(id: $id) {
+    data {
+      id
+      attributes{
+      title,
+        rating,
+        body
+      }
+    }
+  }
+}
+`
+
+export default function ReviewDetails() {
+  const { id } = useParams()
+  const {error, loading, data} = useQuery(REVIEW, {
+    variables: { id: id}
+  });
+
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>Error...</p>
+
+
+  return (
+    <div>
+      <h1>Review Details : {id} </h1>
+        <div className="review-card">
+          <div className="rating">{data.review.data.attributes.rating}</div>
+          <h2>{data.review.data.attributes.title}</h2>
+          
+          <small>console list</small>
+
+          <p>{data.review.data.attributes.body} ...</p>
+        </div>
+    </div>
+  )
+}
